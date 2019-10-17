0b0f478541c03f712f9c551076e13fea4e202cbc
unknown <szpak789@gmail.com>
2019-10-17 12:57:42 +0200

    Zadania

---
+++ Programiki/Zadanie9.py
@@ -0,0 +1,20 @@
+a = 145911
+print(a)
+a = 'Aleksandra Szpakowska'
+print(a)
+b = 145768
+print(b)
+a = 'Marian Alek'
+print(b)
+c = 162911
+print(c)
+c = 'Samuel Bondy'
+print(c)
+d = 148291
+print(d)
+d = 'Klemntyna Super'
+print(d)
+e = 148727
+print(e)
+e = 'Piotr Kowal'
+print(e)
